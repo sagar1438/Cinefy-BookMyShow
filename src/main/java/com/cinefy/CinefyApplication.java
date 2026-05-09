fb34e75a58cd3f7524513a363d3428b4809ee4e8
sagar <sagarpanwer11@gmail.com>
2026-05-09 23:27:47 +0530

    Updated Pom and application file

---
+++ src/main/java/com/cinefy/CinefyApplication.java
@@ -0,0 +1,12 @@
+package com.cinefy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinefyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinefyApplication.class, args);
+    }
+}
