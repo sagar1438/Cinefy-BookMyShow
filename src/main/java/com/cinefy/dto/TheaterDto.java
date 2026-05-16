a27bad1a7f3dc3686f49ed7a9ea2ed3c67107f69
sagar <sagarpanwer11@gmail.com>
2026-05-16 13:14:00 +0530

    Updated TheaterDto file

---
+++ src/main/java/com/cinefy/dto/TheaterDto.java
@@ -0,0 +1,18 @@
+package com.cinefy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TheaterDto {
+    private Long id;
+    private String name;
+    private String address;
+    private String city;
+    private Integer totalScreens;
+}
