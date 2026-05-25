b6da4915bb0481628041b5ff9dda5db961bb0fcf
sagar <sagarpanwer11@gmail.com>
2026-05-25 22:15:11 +0530

    Updated UserRepository File

---
+++ src/main/java/com/cinefy/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.cinefy.repository;
+
+import com.cinefy.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
