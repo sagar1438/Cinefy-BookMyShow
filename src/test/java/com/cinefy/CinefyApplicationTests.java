eae53c0bef6dfb0067f82093af8764a5c40d72b2
sagar <sagarpanwer11@gmail.com>
2026-06-05 01:03:40 +0530

    Updated Test File

---
+++ src/test/java/com/cinefy/CinefyApplicationTests.java
@@ -0,0 +1,12 @@
+package com.cinefy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinefyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
